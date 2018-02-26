57fc4987a636a6d796c2d0da6315558eafb352a7
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-26 22:09:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,15 @@
 def line(arr)
+  new_arr = Array.new
+  count = 0
+  str = ""
   if arr.size == 0
     puts "The line is currently empty."
-
+  else
+    while count < arr.size
+      count +=1
+      str = count + arr[count - 1] 
+      new_arr.push(str)
+  end
+  new_arr
 end
 end
