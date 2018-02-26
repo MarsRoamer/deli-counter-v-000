5403a29788e578aee105a06992d2e581a1580fc5
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-26 22:01:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,6 @@
 def line(arr)
-  new_arr = Array.new
-  count = 1
   if arr.size == 0
     puts "The line is currently empty."
-  else
-    arr.each do |element|
-      new_arr.push(count + element)
-    end
-  end
-    puts "The line is currently #{arr.index(element)} "
+    
+end
 end
