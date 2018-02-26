bbbdc50214fc206ce5f406d62e9c6a17d80d48f6
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-26 22:23:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number 1 in line."
   else
     katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{katz_deli.last} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
   end
 end
